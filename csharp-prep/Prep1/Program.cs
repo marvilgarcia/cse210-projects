6698dd223900ef16db9f05a13029aeb7c679f246
marvilgarcia <marvigarcia015@gmail.com>
2023-03-03 00:10:13 -0400

    Prepare Activities

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        String name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        String lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {name} {lastName}.");
+
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grad percentage? ");
+        String userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+
+        if (percentage >= 90 )
+        {
+            Console.WriteLine("Your grade letter is A.");
+        }
+        else if (percentage >= 80 &&  percentage < 90 )
+        {
+            Console.WriteLine("Your grade letter is B.");
+        }
+        else if (percentage >= 70 &&  percentage < 80 )
+        {
+            Console.WriteLine("Your grade letter is C.");
+        }
+        else if (percentage >= 60 &&  percentage < 70 )
+        {
+            Console.WriteLine("Your grade letter is D.");
+        }
+        else 
+        {
+            Console.WriteLine("Your grade letter is F.");
+        }
+       
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations you have passed the course.");
+        }   
+        else
+        {
+            Console.WriteLine("Sorry, You must take the quiz again");
+        }
+            
     }
+
 }
